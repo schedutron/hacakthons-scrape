2dad1d094ab1255e42500b5c058e12d18402f6ff
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-24 18:19:37 +0530

    Learn scrapy: 8

--- tutorial/tutorial/spiders/quotes_spiders.py
+++ tutorial/tutorial/spiders/quotes_spiders.py
@@ -4,13 +4,11 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, "tag", None)
+        if tag is not None:
+            url = url + "tag/" + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css("div.quote"):
